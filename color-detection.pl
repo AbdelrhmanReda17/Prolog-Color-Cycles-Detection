9d29d1754e72f70eba217b08ae90f0e671198aa7
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2024-04-28 02:39:17 +0300

    add detect cycle predicates and its utilities

--- color-detection.pl
+++ color-detection.pl
@@ -27,3 +27,30 @@ detect_cycles_dfs(Board, Start, Current, Visited, Color, Cycle) :-
         same_color(Start, Next, Board),
         detect_cycles_dfs(Board, Start, Next, [Next|Visited], Color, Cycle)
     ).
+
+% DFS on each node to find all colored cycles
+detect_cycles(Board, Cycle, Color) :-
+    length(Board, NRows),
+    nth0(0, Board, FirstRow),
+    length(FirstRow, NCols),
+    NRows1 is NRows - 1,
+    NCols1 is NCols - 1,
+    between(0, NRows1, X),
+    between(0, NCols1, Y),
+    Start = [X, Y],
+    cell_color(Board, Start, Color),
+    detect_cycles_dfs(Board, Start, Start, [Start], Color, Cycle).
+    
+
+% Predicate to get the color of a cell
+cell_color(Board, [X,Y], Color) :-
+    nth0(X, Board, Row),
+    nth0(Y, Row, Color).
+
+% Check if two cells have the same color
+same_color([X1,Y1], [X2,Y2], Board) :-
+    nth0(X1, Board, Row1),
+    nth0(Y1, Row1, C1),
+    nth0(X2, Board, Row2),
+    nth0(Y2, Row2, C2),
+    C1 == C2.
