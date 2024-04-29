24c27abc81f961bca0c0a45c36b481642ceef095
Abdelrhman Reda <11410120210225@stud.cu.edu.eg>
2024-04-30 00:52:22 +0300

    Update color-detection.pl

--- color-detection.pl
+++ color-detection.pl
@@ -8,20 +8,20 @@ move([X,Y], [X,Y1]) :- Y1 is Y-1, Y1 >= 0. % Defines a move upward from cell [X,
 valid_cell([X,Y], Board) :-
     length(Board, NRows),
     length(Board, NCols),
-    X >= 0, X =< NRows,
-    Y >= 0, Y =< NCols,
-    nth0(X, Board, Row),
-    nth0(Y, Row,_).
+    X >= 0, X <= NRows,
+    Y >= 0, Y <= NCols.
 
 % Depth-first search to find a color cycle starting from a given cell (Start) on the board.
 % It explores neighboring cells with the same color recursively until it forms a cycle or exhausts all possible paths.
 detect_cycles_dfs(Board, Start, Current, Visited, Color, Cycle) :-
     move(Current, Next),
     valid_cell(Next, Board),
-    (   Next == Start,
+    (
+        Next == Start,
         length(Visited, Length),
         Length >= 4 ->
-        reverse(Visited, Cycle)
+        reverse(Visited, Cycle),
+        format("~w cycle found: ~w~n", [Color, Cycle])
     ;
         \+ member(Next, Visited),
         same_color(Start, Next, Board),
@@ -29,7 +29,7 @@ detect_cycles_dfs(Board, Start, Current, Visited, Color, Cycle) :-
     ).
 
 % DFS on each node to find all colored cycles
-detect_cycles(Board, Cycle, Color) :-
+detect_cycles(Board) :-
     length(Board, NRows),
     nth0(0, Board, FirstRow),
     length(FirstRow, NCols),
@@ -37,10 +37,12 @@ detect_cycles(Board, Cycle, Color) :-
     NCols1 is NCols - 1,
     between(0, NRows1, X),
     between(0, NCols1, Y),
-    Start = [X, Y],
-    cell_color(Board, Start, Color),
-    detect_cycles_dfs(Board, Start, Start, [Start], Color, Cycle).
-    
+    cell_color(Board, [X, Y], Color),
+    detect_cycles_dfs(Board, [X, Y], [X, Y], [[X, Y]], Color, _).
+    fail. % Backtrack to find more cycles
+
+detect_cycles(_) :-
+    format("No cycles exist.~n").
 
 % Predicate to get the color of a cell
 cell_color(Board, [X,Y], Color) :-
@@ -49,8 +51,6 @@ cell_color(Board, [X,Y], Color) :-
 
 % Check if two cells have the same color
 same_color([X1,Y1], [X2,Y2], Board) :-
-    nth0(X1, Board, Row1),
-    nth0(Y1, Row1, C1),
-    nth0(X2, Board, Row2),
-    nth0(Y2, Row2, C2),
+    cell_color(Board,[X1,Y1] , C1),
+    cell_color(Board,[X2,Y2] , C2),
     C1 == C2.
